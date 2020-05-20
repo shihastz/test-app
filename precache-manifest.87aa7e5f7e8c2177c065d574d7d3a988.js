e9a75ad6a57d0f532c2edfa5e6571fdf404d855d
shihastz <shihast0503@gmail.com>
2020-05-20 14:26:11 +0530

    Updates

---
+++ precache-manifest.87aa7e5f7e8c2177c065d574d7d3a988.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f1ac6c6eff84ff4dab30c8c87fbf940",
+    "url": "/test-app/index.html"
+  },
+  {
+    "revision": "a951fab9a6270a1340fe",
+    "url": "/test-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "726766461582f92d1fe7",
+    "url": "/test-app/static/js/2.7e6c53c1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-app/static/js/2.7e6c53c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a951fab9a6270a1340fe",
+    "url": "/test-app/static/js/main.119e290d.chunk.js"
+  },
+  {
+    "revision": "42f8bc4754f4b0c9cce4",
+    "url": "/test-app/static/js/runtime-main.847fb34c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
